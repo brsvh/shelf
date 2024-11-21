558092de53e54024ce20567fecaee91e0f9ebf3a
Burgess Chang <bsc@brsvh.org>
2024-11-21 18:09:33 +0800

    chore: back to upstream
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- nix/packages/emacs/manualPackages/typst-ts-mode/package.nix
+++ nix/packages/emacs/manualPackages/typst-ts-mode/package.nix
@@ -10,8 +10,8 @@ trivialBuild {
   version = "0.10.0-2024-11-20";
 
   src = fetchgit {
-    url = "https://codeberg.org/brsvh/typst-ts-mode.git";
-    rev = "bb7ba35dbd06bbe5235d7e5ddbb132c84ce4b119";
+    url = "https://codeberg.org/meow_king/typst-ts-mode.git";
+    rev = "d3e44b5361ed1bbb720a38dafdb29cb8d8b6d8be";
     hash = "sha256-fECXfTjbckgS+kEJ3dMQ7zDotqdxxBt3WFl0sEM60Aw=";
   };
 
